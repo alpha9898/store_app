4126a5adfd3d1d7c079138021ee91b1b011770bd
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2025-04-29 03:28:28 +0300

    refactor(services): centralize API calls in a new helper class

--- store_app/lib/services/all_catagories_service.dart
+++ store_app/lib/services/all_catagories_service.dart
@@ -1,17 +1,10 @@
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:store_app/helper/api.dart';
 
 class AllCatagoriesService {
   Future<List<dynamic>> getAllCatagories() async {
-    http.Response response = await http
-        .get(Uri.parse('https://fakestoreapi.com/products/categories'));
+    List<dynamic> data = await Api()
+        .get(url: 'https://fakestoreapi.com/products/categories');
 
-    if (response.statusCode == 200) {
-      List<dynamic> data = jsonDecode(response.body);
-      return data;
-    } else {
-      throw Exception(
-          'there is proplem with status code ${response.statusCode}'); // Throw an exception if the request fails or there's an issue wit
-    }
+    return data;
   }
 }
--- store_app/lib/services/catagories_servce.dart
+++ store_app/lib/services/catagories_servce.dart
@@ -1,15 +1,11 @@
+import 'package:store_app/helper/api.dart';
 import 'package:store_app/models/product_model.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 class CatagoriesService {
   Future<List<ProductModel>> getCatagoriesProduct(
       {required String catagoryname}) async {
-    http.Response response = await http.get(
-        Uri.parse('https://fakestoreapi.com/products/category/$catagoryname'));
-
-    if (response.statusCode == 200) {
-      List<dynamic> data = jsonDecode(response.body);
+    List<dynamic> data =
+        await Api().get(url: 'https://fakestoreapi.com/products/category/$catagoryname');
 
       List<ProductModel> productsList = [];
       for (int i = 0; i < data.length; i++) {
@@ -19,9 +15,5 @@ class CatagoriesService {
         return productsList;
       }
       return productsList;
-    } else {
-      throw Exception(
-          'there is proplem with status code ${response.statusCode}'); // Throw an exception if the request fails or there's an issue wit
     }
   }
-}
