f95d6894d32bab3dbc15550b8d8fea8c0c3a7048
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2025-04-30 20:06:53 +0300

    working post requst

--- store_app/lib/helper/api.dart
+++ store_app/lib/helper/api.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Api {
   Future<dynamic> get({required String url}) async {
--- store_app/lib/main.dart
+++ store_app/lib/main.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           http.Response response = await http
-              .post(Uri.parse('https://fakestoreapi.com/products '), body: {
+              .post(Uri.parse('https://fakestoreapi.com/products'), body: {
             'title': 'test product',
             'price': '13.5',
             'description': 'lorem ipsum set',
